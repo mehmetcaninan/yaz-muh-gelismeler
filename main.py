55349c56b2dfde209dd44f4a1d5ef1678313edac
mehmetcaninan <133818382+mehmetcaninan@users.noreply.github.com>
2024-11-07 23:29:51 +0300

    GET
    Get isteği yapan endpoint.

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ from flask_restful import Api, Resource, reqparse
 app= Flask(__name__)
 api=Api(app)
 
+class Toplama(Resource):
+    def get(self,ilkSayi,ikinciSayi):
+        toplam=ilkSayi+ikinciSayi
+        return {
+            'toplam':toplam
+        }
+
+
+api.add_resource(Toplama,'/toplama/<int:ilkSayi>/<int:ikinciSayi>')
+
 if __name__ == '__main__':
     app.run()
